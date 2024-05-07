bd1921cb793a49b827869110c5e2493a5098197d
AlekseevDS21 <andrey.alekseev01@list.ru>
2024-05-07 09:37:47 +0300

    Update main
    Signed-off-by: AlekseevDS21 <andrey.alekseev01@list.ru>

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ config = getConfig()
 # Установка соединения с базой данных SQLite
 conn = sqlite3.connect('data.db', check_same_thread=False)  # Добавлен check_same_thread=False для Streamlit
 c = conn.cursor()
+def create_users_table():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT, password TEXT)')
+    conn.commit()
 
+def add_userdata(username, password):
+    c.execute('INSERT INTO userstable(username, password) VALUES (?, ?)', (username, password))
+    conn.commit()
+
+def login_user(username, password):
+    c.execute('SELECT * FROM userstable WHERE username =? AND password = ?', (username, password))
+    data = c.fetchall()
+    return data
+
+def logout_user():
+    st.session_state['authenticated'] = False
+    st.experimental_rerun()
 
 # Изменение структуры таблицы, добавляем столбец для времени
 def create_table():
@@ -74,138 +89,179 @@ def create_pie_chart(data, column='Предсказание'):
 
 
 def main():
-    st.title('Прогнозированипе состояния здоровья лошади')
+    create_users_table()
+    if 'authenticated' not in st.session_state:
+        st.session_state['authenticated'] = False
+    if st.session_state['authenticated']:
+        logout_placeholder = st.empty()
+        st.title('Прогнозирование состояния здоровья животных')
 
-    # Вкладки
-    tab1, tab2, tab3 = st.tabs(['Ввод данных', 'Просмотр БД', 'График'])
+        with logout_placeholder.container():
+            if st.button('Выйти из аккаунта'):
+                logout_user()
 
-    with tab1:
-        st.header('Введите данные для анализа')
+        # Вкладки
+        tab1, tab2, tab3 = st.tabs(['Ввод данных', 'Просмотр БД', 'График'])
 
-        # Описание параметров для ввода
-        parameters_description = {
-            1: "Возраст лошади (лет):",
-            2: "Температура тела (градусы Цельсия):",
-            3: "Частота пульса (удары в минуту):",
-            4: "Частота пульса (удары в минуту):",
-            5: "Частота пульса (удары в минуту):",
-            6: "Частота пульса (удары в минуту):",
-            7: "Частота пульса (удары в минуту):",
-            8: "Частота пульса (удары в минуту):",
-            9: "Частота пульса (удары в минуту):",
-            10: "Частота пульса (удары в минуту):",
-            11: "Частота пульса (удары в минуту):",
-            12: "Частота пульса (удары в минуту):",
-            13: "Частота пульса (удары в минуту):",
-            14: "Частота пульса (удары в минуту):",
-            15: "Частота пульса (удары в минуту):",
-            16: "Частота пульса (удары в минуту):",
-            17: "Частота пульса (удары в минуту):",
-            18: "Частота пульса (удары в минуту):",
-            19: "Частота пульса (удары в минуту):",
-            20: "Частота пульса (удары в минуту):",
-            21: "Частота пульса (удары в минуту):",
-            22: "Частота пульса (удары в минуту):",
-            23: "Частота пульса (удары в минуту):",
-            24: "Частота пульса (удары в минуту):",
-            25: "Частота пульса (удары в минуту):",
-            26: "Частота пульса (удары в минуту):",
-            27: "Частота пульса (удары в минуту):",
-            28: "Общий уровень белка (г/дл):",
-            29: "Рефлекс перистальтики (баллы):"
-        }
+        with tab1:
+            st.header('Введите данные для анализа')
 
-        user_input_list = []
-        valid_data = True
+            # Описание параметров для ввода
+            parameters_description = {
+                1: "ID лошади",
+                2: "Предыдущее посещение (номер, 0 если нет)",
+                3: "Объем упакованных клеток (PCV):",
+                4: "Общий белок(г/л):",
+                5: "Частота пульса (уд/мин)",
+                6: "Ректальная температура (°C)",
+                7: "Белок брюшной полости(г/л)",
+                8: "pH назогастрального рефлюкса",
+                9: "Ввести номер соответствующего значения \n ('alert': 0, 'depressed': 1, 'moderate': 2, 'mild_pain': 3, 'severe_pain': 4, 'extreme_pain': 5)",
+                10: "Назогастральный рефлюкс ('less_1_liter': 0, 'none': 1, 'more_1_liter': 2)",
+                11: "Ректальный осмотр фекалий ('absent': 0, 'decreased': 1, 'normal': 2, 'increased': 3)",
+                12: "Частота пульса (удары в минуту):",
+                13: "Частота пульса (удары в минуту):",
+                14: "Частота пульса (удары в минуту):",
+                15: "Частота пульса (удары в минуту):",
+                16: "Частота пульса (удары в минуту):",
+                17: "Частота пульса (удары в минуту):",
+                18: "Частота пульса (удары в минуту):",
+                19: "Частота пульса (удары в минуту):",
+                20: "Частота пульса (удары в минуту):",
+                21: "Частота пульса (удары в минуту):",
+                22: "Частота пульса (удары в минуту):",
+                23: "Частота пульса (удары в минуту):",
+                24: "Частота пульса (удары в минуту):",
+                25: "Частота пульса (удары в минуту):",
+                26: "Частота пульса (удары в минуту):",
+                27: "Частота пульса (удары в минуту):",
+                28: "Общий уровень белка (г/дл):",
+                29: "Рефлекс перистальтики (баллы):"
+            }
 
-        # Создаем форму для ввода данных
-        with st.form("input_form"):
-            for i in range(1, 30):
-                description = parameters_description.get(i, f"Параметр {i}:")
-                if i in [3, 4, 5, 6, 7, 8, 9, 28]:
-                    # Для параметров, представляющих числа с плавающей точкой
-                    param = st.text_input(description, key=f'param{i}')
-                else:
-                    # Для параметров, представляющих целые числа
-                    param = st.text_input(description, key=f'param{i}')
+            user_input_list = []
+            valid_data = True
 
-            submit_button = st.form_submit_button(label='Обработать')
+            # Создаем форму для ввода данных
+            with st.form("input_form"):
+                for i in range(1, 30):
+                    description = parameters_description.get(i, f"Параметр {i}:")
+                    if i in [3, 4, 5, 6, 7, 8, 9, 28]:
+                        # Для параметров, представляющих числа с плавающей точкой
+                        param = st.text_input(description, key=f'param{i}')
+                    else:
+                        # Для параметров, представляющих целые числа
+                        param = st.text_input(description, key=f'param{i}')
 
-        # Кнопка для обработки ввода
-        if submit_button:
-            for i in range(1, 30):
-                user_input_list.append(st.session_state[f'param{i}'])
-            print(user_input_list)
-            old_value = user_input_list[28]
-            # Заменяем 29-е значение на 'единица'
-            user_input_list[28] = '1'
-            # Добавляем старое значение в конец списка
-            user_input_list.append(old_value)
-            # Обработка введенных данных
-            # Здесь код для обработки данных
-            with open("hist_model.pkl", 'rb') as file:
-                loaded_model = pickle.load(file)
-            prediction = loaded_model.predict([user_input_list])
+                submit_button = st.form_submit_button(label='Обработать')
 
-            outcome_mapping = {0: 'died', 1: 'euthanized', 2: 'lived'}
-            # Вывод результата и запись в базу данных
-            prediction = outcome_mapping[prediction[0]]
-            st.write('Результат предсказания:', prediction)
-            create_table()
-            add_data(*user_input_list, prediction)
+            # Кнопка для обработки ввода
+            if submit_button:
+                for i in range(1, 30):
+                    user_input_list.append(st.session_state[f'param{i}'])
+                print(user_input_list)
+                old_value = user_input_list[28]
+                # Заменяем 29-е значение на 'единица'
+                user_input_list[28] = '1'
+                # Добавляем старое значение в конец списка
+                user_input_list.append(old_value)
+                # Обработка введенных данных
+                # Здесь код для обработки данных
+                with open("hist_model.pkl", 'rb') as file:
+                    loaded_model = pickle.load(file)
+                prediction = loaded_model.predict([user_input_list])
 
-    with tab2:
-        st.header('Просмотр данных')
-        password = st.text_input('Введите пароль для доступа к данным:', type='password')
-        correct_password = config['password']  # Замените 'секрет' на ваш реальный пароль
+                outcome_mapping = {0: 'смерть', 1: 'эвтаназия', 2: 'живой'}
+                # Вывод результата и запись в базу данных
+                prediction = outcome_mapping[prediction[0]]
+                st.write('Результат предсказания:', prediction)
+                create_table()
+                add_data(*user_input_list, prediction)
 
-        if password == correct_password:
-            # Фильтр по предсказанию
-            c.execute('SELECT DISTINCT prediction FROM results')
-            unique_predictions = c.fetchall()
-            prediction_options = [pred[0] for pred in unique_predictions]
+        with tab2:
+            st.header('Просмотр данных')
+            password = st.text_input('Введите пароль для доступа к данным:', type='password')
+            correct_password = config['password']
 
-            selected_prediction = st.selectbox('Фильтр по полю predict:', ['Все'] + prediction_options)
-            # Фильтр по дате
-            st.subheader('Фильтр по дате')
-            start_date = st.date_input('С:', date.today())
-            end_date = st.date_input('По:', date.today())
+            if password == correct_password:
+                # Фильтр по предсказанию
+                c.execute('SELECT DISTINCT prediction FROM results')
+                unique_predictions = c.fetchall()
+                prediction_options = [pred[0] for pred in unique_predictions]
 
-            if st.button('Показать данные'):
-                # Получение отфильтрованных данных
-                data = view_data(start_date, end_date, selected_prediction)
-                # Вывод данных
-                data_df = pd.DataFrame(data, columns=['hospital_number', 'lesion_1', 'packed_cell_volume',
-                    'total_protein', 'pulse', 'rectal_temp',
-                    'respiratory_rate', 'abdomo_protein', 'nasogastric_reflux_ph',
-                    'pain', 'nasogastric_reflux', 'rectal_exam_feces',
-                    'abdominal_distention', 'abdomo_appearance', 'abdomen',
-                    'nasogastric_tube', 'temp_of_extremities', 'peristalsis',
-                    'cp_data', 'capillary_refill_time', 'surgery',
-                    'peripheral_pulse', 'mucous_membrane_bright_red', 'surgical_lesion',
-                    'mucous_membrane_pale_pink', 'mucous_membrane_normal_pink',
-                    'mucous_membrane_pale_cyanotic', 'age', 'is_generated',
-                    'mucous_membrane_bright_pink', 'Предсказание', 'Дата'])
-                st.dataframe(data_df)
-        else:
-            if st.button('Проверить пароль'):
-                st.error('Неправильный пароль!')
-    with tab3:
-        st.header('Статистика предсказаний')
-        password = st.text_input('Введите пароль для доступа к статистике:', type='password', key='password_tab3')
-        correct_password = config['password']  # Замените 'секрет' на ваш реальный пароль
+                selected_prediction = st.selectbox('Фильтр по полю predict:', ['Все'] + prediction_options)
+                # Фильтр по дате
+                st.subheader('Фильтр по дате')
+                start_date = st.date_input('С:', date.today())
+                end_date = st.date_input('По:', date.today())
 
-        if password == correct_password:
-            # Получаем данные для диаграммы
-            c.execute('SELECT prediction FROM results')
-            all_predictions = c.fetchall()
-            predictions_df = pd.DataFrame(all_predictions, columns=['Предсказание'])
+                if st.button('Показать данные'):
+                    # Получение отфильтрованных данных
+                    data = view_data(start_date, end_date, selected_prediction)
+                    # Вывод данных
+                    data_df = pd.DataFrame(data, columns=['hospital_number', 'lesion_1', 'packed_cell_volume',
+                        'total_protein', 'pulse', 'rectal_temp',
+                        'respiratory_rate', 'abdomo_protein', 'nasogastric_reflux_ph',
+                        'pain', 'nasogastric_reflux', 'rectal_exam_feces',
+                        'abdominal_distention', 'abdomo_appearance', 'abdomen',
+                        'nasogastric_tube', 'temp_of_extremities', 'peristalsis',
+                        'cp_data', 'capillary_refill_time', 'surgery',
+                        'peripheral_pulse', 'mucous_membrane_bright_red', 'surgical_lesion',
+                        'mucous_membrane_pale_pink', 'mucous_membrane_normal_pink',
+                        'mucous_membrane_pale_cyanotic', 'age', 'is_generated',
+                        'mucous_membrane_bright_pink', 'Предсказание', 'Дата'])
+                    st.dataframe(data_df)
+            else:
+                if st.button('Проверить пароль'):
+                    st.error('Неправильный пароль!')
+        with tab3:
+            st.header('Статистика предсказаний')
+            password = st.text_input('Введите пароль для доступа к статистике:', type='password', key='password_tab3')
+            correct_password = config['password']  # Замените 'секрет' на ваш реальный пароль
 
-            # Создаем и отображаем круговую диаграмму
-            create_pie_chart(predictions_df)
-        else:
-            if st.button('Проверить пароль', key='check_password_tab3'):
-                st.error('Неправильный пароль!')
+            if password == correct_password:
+                # Получаем данные для диаграммы
+                c.execute('SELECT prediction FROM results')
+                all_predictions = c.fetchall()
+                predictions_df = pd.DataFrame(all_predictions, columns=['Предсказание'])
+
+                # Создаем и отображаем круговую диаграмму
+                create_pie_chart(predictions_df)
+            else:
+                if st.button('Проверить пароль', key='check_password_tab3'):
+                    st.error('Неправильный пароль!')
+
+    else:
+        with st.sidebar:
+            st.subheader("Регистрация и вход")
+
+            menu = ["Вход", "Регистрация"]
+            choice = st.selectbox("Меню", menu, key="menu_selectbox")
+
+            if choice == "Регистрация":
+                new_username = st.text_input("Имя пользователя", key="new_username")
+                new_password = st.text_input("Пароль", type='password', key="new_password")
+                confirm_password = st.text_input("Повторите пароль", type='password', key="confirm_password")
+
+                if new_password and confirm_password and st.button("Зарегистрироваться", key="signup_button"):
+                    # Проверяем, совпадают ли пароли
+                    if new_password == confirm_password:
+                        add_userdata(new_username, new_password)
+                        st.success("Вы успешно зарегистрировались!")
+                    else:
+                        st.error("Пароли не совпадают. Попробуйте снова.")
+
+            elif choice == "Вход":
+                username = st.text_input("Имя пользователя", key="username_login")
+                password = st.text_input("Пароль", type='password', key="password_login")
+
+                if username and password and st.button("Войти", key="login_button"):
+                    if login_user(username, password):
+                        st.success("Вы успешно вошли в систему!")
+                        st.session_state['authenticated'] = True
+                        st.experimental_rerun()
+                    else:
+                        st.error("Неверное имя пользователя или пароль")
 
 
 if __name__ == '__main__':
